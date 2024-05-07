24a05e4ed363fbb7ce91a633000a7f9e37e10d94
Edouard26 <edouard.halimi@gmail.com>
2024-05-07 15:28:09 +0200

    Q5

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
-str = "Holberton School"
-:
-=======
-#!/usr/bin/python3
-str = "Holberton School"
+str = "Holberton School
 print(str * 3)
 print(str[:9])
->>>>>>> 5fe1f189d7730aa21e8e56c300f866e581a042b8
