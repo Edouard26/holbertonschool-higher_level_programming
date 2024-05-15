478806e9fd72930b7d20352c643d38c73c18977f
Edouard26 <edouard.halimi@gmail.com>
2024-05-15 10:56:21 +0200

    Q10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,5 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or not a_dictionary:
         return None
-    
     max_key = max(a_dictionary, key=a_dictionary.get)
     return max_key
