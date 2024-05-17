c0262ad52741f7b43a7b677c80cebc60810a441b
Edouard26 <edouard.halimi@gmail.com>
2024-05-17 16:08:30 +0200

    Q2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""Defines a square """
+"""Defines a square."""
 
 
 class Square:
-    """Defines a square """
+    """Defines a square"""
     def __init__(self, size=0):
+        """Initialize data.
+
+        Args:
+            size (int, optional): Size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
-
-        @property
-        def size(self):
-            return self.__size
-
-        @size.setter
-        def size(self, value):
-            if not insinstance(value, int):
-                raise Type Error("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
