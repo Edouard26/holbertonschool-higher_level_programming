8964729574cc1b2afeb774a2327764d124a1b0dc
Edouard26 <edouard.halimi@gmail.com>
2024-05-17 15:53:54 +0200

    correction pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Defines a square."""
 
+
 class Square:
     """Defines a square. """
     __size = None
-    
+
     def __init__(self, size):
         """Initialize self and size of the square"""
         self.__size = size
