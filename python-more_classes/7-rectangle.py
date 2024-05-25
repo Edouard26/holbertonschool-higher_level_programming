45f536aa6e1702708ca749c4b0f2700e8c14323b
Edouard26 <edouard.halimi@gmail.com>
2024-05-25 18:02:36 +0200

    Q7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -76,7 +76,7 @@ class Rectangle:
             return ""
         rect_str = ""
         for _ in range(self.height):
-            rect_str += '#' * self.width + '\n'
+            rect_str += str(self.print_symbol) * self.width + '\n'
         return rect_str[:-1]
 
     def __repr__(self):
