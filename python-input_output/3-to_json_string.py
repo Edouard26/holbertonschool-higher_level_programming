86cbc266542bde661e763ddbbac8b1fac21bd229
Edouard26 <edouard.halimi@gmail.com>
2024-05-27 13:38:33 +0200

    Q3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -7,5 +7,10 @@
 import json
 
 def to_json_string(my_obj):
+    """
+        Returns the JSON representation of an object (string)
+        Arguments:
+            my_obj: Object
+    """
     return json.dumps(my_obj)
 
