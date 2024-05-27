429f39a6549bff10c57ff8f394ed7f1eae8103b1
Edouard26 <edouard.halimi@gmail.com>
2024-05-27 14:57:10 +0200

    Q8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,7 +5,7 @@
 
 
 def class_to_json(obj):
-     """
+    """
     Returns a dictionary description suitable for JSON serialization of an object.
 
     Args:
@@ -14,6 +14,7 @@ def class_to_json(obj):
     Returns:
         dict: A dictionary containing serializable attributes of the object.
     """
+
     json_dict = {}
     for attr, value in obj.__dict__.items():
         if isinstance(value, (list, dict, str, int, bool)):
