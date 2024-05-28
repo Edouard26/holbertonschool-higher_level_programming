69aed3637117cdd1056f70c2e67b6e14dc31a9ce
Edouard26 <edouard.halimi@gmail.com>
2024-05-28 19:24:06 +0200

    Q0

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,21 @@
+import json
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serialize Python dictionnary and save it to a JSON file
+
+    :param data: A Python dictionary and save it to a JSON file.
+    :param data: A Python Dictionary with data
+    :param filename: The filename of the output JSON file
+    """
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+def load_and_deserialize(filename):
+    """
+    Load and deserialize data from a JSON file to a Python dictionary.
+    :param filename: The filename of the input JSON file.
+    :return: A Python Dictionary with the deserialized JSON data from the file.
+    """
+    with open(filename, 'r') as json_file:
+        data = json.load(json_file)
+    return data
