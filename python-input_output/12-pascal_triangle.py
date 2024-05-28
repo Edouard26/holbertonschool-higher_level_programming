04469cf7af54f28e4069a08ea57e9d25f1297d5a
Edouard26 <edouard.halimi@gmail.com>
2024-05-28 12:16:49 +0200

    Q12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,7 +6,7 @@ Defines a function that returns Pascal's triangle up to n rows
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal’s triangle of n.
+    Returns list of lists of integers representing Pascal’s triangle of n
     """
     if n <= 0:
         return []
