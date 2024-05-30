a9fa240201286f19a38ff8d40b92a017a774aaea
Edouard26 <edouard.halimi@gmail.com>
2024-05-30 17:24:26 +0200

    fetching and processing of data from the JSONPlaceHolder API and the creation of a CSV file

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,32 +1,28 @@
-#!/usr/bin/env python3
 import requests
 import csv
-
 def fetch_and_print_posts():
-    url = "https://jsonplaceholder.typicode.com/posts"
-    response = requests.get(url)
-    print(f"Status Code: {response.status_code}")
+    
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')    
+    print(f'Status Code: {response.status_code}')    
 
-    if response.status_code == 200:
-        posts = response.json()
+    if response.status_code == 20
+    0:
+        posts = response.json()        
+
         for post in posts:
             print(post['title'])
 
 def fetch_and_save_posts():
-    url= "https://jsonplaceholder.typicode.com/posts"
-    response = requests.get(url)
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')    
 
     if response.status_code == 200:
-        posts = response.json()
-        posts_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} 
-    for post in posts]        
-    headers = ['id', 'title', 'body']
+        posts = response.json()        
+        posts_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]        
 
-    with open('posts.csv', mode='w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
-        writer.writeheader
-        writer.writerows(structured_data)
+        headers = ['id', 'title', 'body']        
 
-    if __name__ == '__main__':
-        fetch_and_print_posts()
-        fetch_and_save_posts()
+        with open('posts.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writeheader()
+            writer.writerows(posts_data)
+
