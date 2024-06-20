15ee9e50c5a521e717b80e3718b9a7df88c81a66
Edouard26 <edouard.halimi@gmail.com>
2024-06-20 11:24:25 +0200

    Q1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,13 +1,5 @@
 -- create_user.sql
-
--- Create user 'user_0d_1'@'localhost' if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Ensure the user has the correct password
 ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to 'user_0d_1'@'localhost'
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Flush privileges to ensure that the changes take effect
 FLUSH PRIVILEGES;
