85e39199aef4a2b73847e4b26d7ffcb9b0661045
Edouard26 <edouard.halimi@gmail.com>
2024-06-20 11:58:53 +0200

    Q1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,7 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_1';
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL
+ON classicmodels.* 
+TO MySQL
