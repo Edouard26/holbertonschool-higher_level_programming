cc3ad5f71bf7bc4437e650836d1f50d1b5b63c74
Edouard26 <edouard.halimi@gmail.com>
2024-06-20 11:25:30 +0200

    Q1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- create_user.sql
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
