7357fb83c0428da5d5702254da2a003b4545d58b
Edouard26 <edouard.halimi@gmail.com>
2024-06-20 11:14:18 +0200

    Q0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,4 @@
--- 0-privileges.sql
-
--- List privileges for user_0d_1
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
