a3685afab2b2dc2bb9f274bd53e6cac9ff3d1d9f
Edouard26 <edouard.halimi@gmail.com>
2024-06-21 18:07:33 +0200

    Q5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,2 @@
---creates the table unique_id
--- aaaa
-CREATE TABLE IF NOT EXISTS unique_id (
-	id INT DEFAULT 1 UNIQUE,
-	name VARCHAR(256)
-);
+-- Creates the table unique_id
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
