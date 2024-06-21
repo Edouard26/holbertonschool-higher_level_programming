e510d1ea637058396b5abaf1422f7c8b5ecdfc98
Edouard26 <edouard.halimi@gmail.com>
2024-06-21 17:45:53 +0200

    Q8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,26 @@
--- Select and list all cities of California
-
 USE hbtn_0d_usa;
 
-SELECT cities.name 
-FROM cities, states 
-WHERE states.name = 'California' AND cities.state_id = states.id 
+-- Ensure states exist
+INSERT IGNORE INTO states (name)
+VALUES ('California'), ('Arizona'), ('Texas'), ('Utah');
+
+-- Insert cities
+INSERT INTO cities (state_id, name)
+VALUES
+    ((SELECT id FROM states WHERE name = 'California'), 'San Jose'),
+    ((SELECT id FROM states WHERE name = 'California'), 'San Francisco'),
+    (NULL, 'Page'),
+    (NULL, 'Houston'),
+    (NULL, 'Dallas'),
+    (NULL, 'Paris');
+
+-- List all states
+SELECT id, name
+FROM states;
+
+-- List all cities of California, Arizona, Texas, and Utah
+SELECT cities.name
+FROM cities
+WHERE cities.state_id IN (SELECT id FROM states WHERE name IN ('California', 'Arizona', 'Texas', 'Utah'))
 ORDER BY cities.id ASC;
+
