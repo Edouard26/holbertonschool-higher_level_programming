8f4b5c777af89a635ae0ea0af45947a8058afde8
Edouard26 <edouard.halimi@gmail.com>
2024-07-15 13:36:39 +0200

    mv content of main-2 to task_02

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,1 +1,16 @@
- 
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/items')
+def items():
+    # Load items from items.json
+    with open('items.json') as f:
+        data = json.load(f)
+    
+    # Pass the list of items to the template
+    return render_template('items.html', items=data['items'])
+
+if __name__ == '__main__':
+    app.run(debug=True) 
